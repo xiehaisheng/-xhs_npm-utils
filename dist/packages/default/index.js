f55cdafc1fe61c9c2ee1811c85397b70a69d4ff0
鑫鹏 <meet201610@163.com>
2022-08-26 11:12:48 +0800

    gisData-4

--- dist/packages/default/index.js
+++ dist/packages/default/index.js
@@ -1,6 +1,6 @@
 import * as Util from '../common/util.js';
 import * as Env from '../common/env.js';
 import * as Url from '../common/url.js';
-import * as GisData from '../common/gisData';
-console.log(GisData, 'GisData-----');
+import * as GisData from '../common/gisData.js';
+
 export { Util, Env, Url, GisData };
